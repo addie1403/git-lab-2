5b74854559fbb0ed1a7d6e7e58ad62f4a8eef73c
Addie <as244420@ohio.edu>
2021-09-03 17:24:41 -0400

    git-lab-2

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Addie Smith>
+ *        Date: <9.3.21>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
